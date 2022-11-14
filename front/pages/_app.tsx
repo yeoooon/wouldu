b1ea22a9fb98585d33251340c9b4c781dd908ea9
남궁혜진 <nkhj0404@gmail.com>
2022-11-14 21:44:22 +0900

    feat: Prettier 적용

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,6 +1,5 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Head from "next/head";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
 
