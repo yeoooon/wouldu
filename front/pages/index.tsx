7712f39106835765794e8c64bbf0bf6927e7f808
yeoooon <katkrar@naver.com>
2022-11-21 15:14:25 +0900

    feat: 소개 페이지에서 로그인 페이지로 라우팅 구현

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,5 +1,11 @@
+import Link from 'next/link';
+
 import Seo from "../components/Seo";
 import styled from "styled-components";
+import { GlobalStyle } from "../styles/global-style";
+import { colors, fontSize, borderSize } from "../styles/common_style";
+import { darkTheme, lightTheme } from "../styles/theme";
+import { Wrapper, Container, Box } from "../styles/layout";
 
 // const Home: NextPage = () => {
 //   return (
@@ -14,12 +20,35 @@ export default function Home() {
   return (
     <>
       <Seo title="Home" />
-      <Test>Home</Test>
+      <Wrapper>
+        <Header>
+          <nav>
+            <Link href='/'>LOGO</Link>
+            <Link href='/login'><button>서비스 이용하러 가기</button></Link>
+          </nav>
+        </Header>
+      <div>
+        서비스 소개가 들어갈 공간
+      </div>
+      </Wrapper>
     </>
   );
 }
 
-const Test = styled.div`
+const Header = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  max-width: 100vw;
+  z-index: 999;
+  height: 64px;
   background-color: ${props => props.theme.color.background};
   font-size: ${props => props.theme.fontSize.textLg};
+
+  nav {
+    display: flex;
+    justify-content: space-between;
+    padding: 0.5rem 1.5rem;
+  }
 `;
