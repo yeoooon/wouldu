f46566d8b52e74983d5a98b41a03163040fdbe68
남궁혜진 <nkhj0404@gmail.com>
2022-11-23 15:15:18 +0900

    feat: 회원탈퇴 api

--- front/services/api/user.ts
+++ front/services/api/user.ts
@@ -1,5 +1,4 @@
 import { UserJoinForm, UserLoginForm } from "@type/user";
-import axios from "axios";
 import { axiosInstance } from "./axiosInstance";
 
 //회원가입
@@ -24,6 +23,20 @@ export const requestLogin = async (loginInfo: UserLoginForm) => {
   try {
     const { data } = await axiosInstance.post("user/join", requestData);
     sessionStorage.setItem("userToken", data.token);
+    return data;
+  } catch (err) {
+    console.log(err);
+    // if (axios.isAxiosError(err) && err.response?.status === 400) {
+    //   return err.response.status;
+    // }
+  }
+};
+
+//회원탈퇴
+export const deleteUser = async () => {
+  try {
+    const { data } = await axiosInstance.post("user/delete");
+    sessionStorage.removeItem("userToken");
     return data;
   } catch (err) {
     console.log(err);
