ecd68698d2f7fc1c9deee0c5644c8531b31f64ec
남궁혜진 <nkhj0404@gmail.com>
2022-11-23 14:39:23 +0900

    feat:회원가입 api 작성

--- front/services/api/user.ts
+++ front/services/api/user.ts
@@ -1,5 +1,18 @@
-const user = () => {
-  return null;
-};
+import { UserJoinForm } from "@type/user";
+import axios from "axios";
+import { axiosInstance } from "./axiosInstance";
 
-export { user };
+//회원가입
+export const userJoin = async (joinInfo: UserJoinForm) => {
+  const requestData = JSON.stringify(joinInfo);
+
+  try {
+    const { data } = await axiosInstance.post("", requestData);
+    sessionStorage.setItem("userToken", data.token);
+    return data;
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      alert("이메일 또는 비밀번호가 일치하지 않습니다.");
+    }
+  }
+};
