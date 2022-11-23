7daf04e831903ba120ce22e2ac23dd78994cfe09
남궁혜진 <nkhj0404@gmail.com>
2022-11-23 17:13:51 +0900

    feat: axios interceptor 적용

--- front/services/api/axiosInstance.ts
+++ front/services/api/axiosInstance.ts
@@ -3,36 +3,26 @@ import { config } from "process";
 
 export const BASE_URL = `http://${window.location.hostname}:5001`;
 
-export const axiosInstance = axios.create({});
-
-axiosInstance.defaults.baseURL = BASE_URL;
-axiosInstance.defaults.timeout = 3000;
-axiosInstance.defaults.headers.put["Content-Type"] = "application/json";
-axiosInstance.defaults.headers.post["Content-Type"] = "application/json";
-axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${sessionStorage.getItem("userToken")}`;
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 3000,
+});
 
 axiosInstance.interceptors.request.use(
-  async config => ({
-    // 요청을 보내기 전에 수행할 일
-    ...config,
-    headers: {},
-  }),
+  async config => {
+    config.headers = config.headers ?? {};
+
+    //요청을 보내기 전에 수행할 일
+    if (config.data instanceof FormData) {
+      config.headers["Content-Type"] = "multipart/form-data";
+    } else {
+      config.headers["Content-Type"] = "application/json";
+    }
+    config.headers.Authorization = `Bearer ${sessionStorage.getItem("userToken")}`;
+    return config;
+  },
   error => {
     // 오류 요청을 보내기전 수행할 일
-    // ...
-    return Promise.reject(error);
-  },
-);
-
-// 응답 인터셉터 추가
-axiosInstance.interceptors.response.use(
-  function (response) {
-    // 응답 데이터를 가공
-    // ...
-    return response;
-  },
-  function (error) {
-    // 오류 응답을 처리
     // ...
     return Promise.reject(error);
   },
