6bd89021c1d7e774085a94d275e5d4175167d675
zzeong <zzeong315@gamil.com>
2022-11-24 11:52:10 +0900

    fix: calendar error

--- front/components/page/planner/Calendar.tsx
+++ front/components/page/planner/Calendar.tsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
-import ReactCalendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css'; // css import
-
-const Calendar = () => {
-  const [value, onChange] = useState(new Date());
-
-  return (
-    <div>
-      <ReactCalendar onChange={onChange} value={value} />
-    </div>
-  );
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+  
+const ReactCalendar = () =>  {
+    const [value, onChange] = useState(new Date());
+  
+    return (
+        <div>
+            <Calendar
+                onChange={onChange}
+                value={value}
+            />
+        </div>
+    );
 }
 
-export default Calendar;
+export default ReactCalendar;
