323632f7b76647c597db479637b89747dca5e3ae
chaemin <chaemins193@gmail.com>
2022-11-24 18:28:26 +0900

    feat: friend request initial setting

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FriendModule } from './friend/friend.module';
 
 @Module({
-  imports: [],
+  imports: [FriendModule],
   controllers: [AppController],
   providers: [AppService],
 })
