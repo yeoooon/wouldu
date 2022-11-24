e17333813bf064371cd9a5d2bf743427c90c9bdd
chaemin <chaemins193@gmail.com>
2022-11-25 00:24:20 +0900

    fix: status -> registerProgress 변수명 변경

--- back/src/user/entities/user.entity.ts
+++ back/src/user/entities/user.entity.ts
@@ -29,7 +29,7 @@ export class User {
   signupVerifyToken: string;
 
   @Column()
-  status: string;
+  registerProgress: string;
 
   @Column({
     type: 'timestamp',
--- back/src/user/user.module.ts
+++ back/src/user/user.module.ts
@@ -4,11 +4,21 @@ import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { EmailService } from 'src/email/email.service';
+import { AuthService } from 'src/auth/auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: {
+        expiresIn: '60s',
+      },
+    }),
+  ],
   controllers: [UserController],
-  providers: [UserService, EmailService],
+  providers: [UserService, EmailService, AuthService],
   exports: [TypeOrmModule],
 })
 export class UserModule {}
