6fc120a1258da6383fa3668e65e5858c1b484439
남궁혜진 <nkhj0404@gmail.com>
2022-12-06 13:06:11 +0900

    feat: recoil로 친구상태 관리하던 부분 삭제 함수분리

---
+++ front/services/utils/useGetFriend.ts
@@ -0,0 +1,33 @@
+import { userAtom } from "@recoil/user";
+import { getFriend } from "@services/api/friend";
+import { useQuery } from "@tanstack/react-query";
+import { Friend, FriendInfo } from "@type/friend";
+import { useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
+
+export const useGetFriend = () => {
+  const [isConnected, setIsConnected] = useState<boolean>();
+
+  const user = useRecoilValue(userAtom);
+  const { data: friendInfo } = useQuery<Friend[]>(["friend", "info"], () => getFriend());
+  const [friend, setFriend] = useState<FriendInfo>();
+
+  useEffect(() => {
+    if (friendInfo && friendInfo.length >= 1) {
+      console.log("친구있음확인");
+      const data = friendInfo.find(info => info?.toUserId === user?.id);
+      setFriend({
+        id: data?.fromUserId!,
+        title: data?.title!,
+        createdAt: data?.createdAt!,
+        nickname: data?.fromUser.nickname!,
+      });
+      setIsConnected(true);
+    } else {
+      setIsConnected(false);
+    }
+    console.log("getFriend", friendInfo);
+  }, [friendInfo]);
+
+  return { isConnected, data: friend };
+};
