ce1330a3bd60fdd08cc6efada1cd5f9cf0be8dc8
남궁혜진 <nkhj0404@gmail.com>
2022-12-06 12:14:18 +0900

    test: 훅 테스트중

--- front/components/page/mypage/EditConnection.tsx
+++ front/components/page/mypage/EditConnection.tsx
@@ -2,8 +2,6 @@ import { useEffect, useState } from "react";
 
 import BeforeConnect from "./BeforeConnect";
 import AfterConnect from "./AfterConnect";
-import { getFriend } from "@services/api/friend";
-import { useQuery } from "@tanstack/react-query";
 import { useRecoilValue } from "recoil";
 import { isConnectedFriendAtom } from "@recoil/friend";
 
