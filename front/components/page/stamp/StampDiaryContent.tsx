8726ec25f10d7e99db63cfd9328e1f10f9ac2cf4
yeoooon <katkrar@naver.com>
2022-12-07 17:34:11 +0900

    feat: stamp 페이지 친구 없는 경우 처리

--- front/components/page/stamp/StampDiaryContent.tsx
+++ front/components/page/stamp/StampDiaryContent.tsx
@@ -10,12 +10,15 @@ import { isUserDiary, isPartnerDiary } from "@services/utils/diaryAuthor";
 import UserDiary from "../diary/UserDiary";
 import PartnerDiary from "../diary/PartnerDiary";
 import { useGetDiary } from "@services/utils/useGetDiary";
+import { useGetFriend } from "@services/utils/useGetFriend";
 
 const StampDiaryContent = () => {
   const [todayDiary, setTodayDiary] = useState<Array<Diary> | undefined>([]);
+  const { isConnected } = useGetFriend();
 
   const todayDate = useRecoilValue(today);
-  console.log(useGetDiary(todayDate));
+  
+  const { userDiary, partnerDiary } = useGetDiary(todayDate);
 
   // 연결 x -> 먼저 일기를 친구와 연결해 보세요! ->
   // 연결 o -> 다이어리 o -> content 보여 주기
@@ -26,14 +29,14 @@ const StampDiaryContent = () => {
       <DiarySummary>
         <Name>나</Name>
         <Content>
-            {todayDiary && todayDiary.length > 0? todayDiary.find(isUserDiary)!.content : <p>작성된 일기가 없어요.</p>}
+            {!isConnected? <p>먼저 친구와 연결해 보세요!</p> : userDiary?.content}
           </Content>
       </DiarySummary>
       <PartnerDiarySummary>
         <PartnerName>상대</PartnerName>
         <Content>
-            {todayDiary && todayDiary.length > 0? todayDiary.find(isPartnerDiary)!.content : <p>작성된 일기가 없어요.</p>}
-          </Content>
+          {!isConnected? <p>먼저 친구와 연결해 보세요!</p> : partnerDiary?.content}
+        </Content>
       </PartnerDiarySummary>
     </ContentBox>
   );
--- front/services/utils/useGetDiary.ts
+++ front/services/utils/useGetDiary.ts
@@ -8,35 +8,42 @@ export const useGetDiary = (yyyymmdd: string) => {
   // 일기 데이터를 찾고 싶은 날짜 ('yyyy-mm-dd' 형태) 를 인자로 받습니다.
 
   const [diaryName, setDiaryName] = useState<string>('');
-  const [userDiary, setUserDiary] = useState<Diary | undefined>();
-  const [partnerDiary, setPartnerDiary] = useState<Diary | undefined>();
+  const [userDiary, setUserDiary] = useState<Diary>({
+    title: '',
+    nickname: '',
+    content: '작성된 내용이 없습니다.',
+  });
+  const [partnerDiary, setPartnerDiary] = useState<Diary>({
+    title: '',
+    nickname: '',
+    content: '작성된 내용이 없습니다.',
+  });
 
   const { data } = useQuery(["diaries", yyyymmdd], () => getDiary(yyyymmdd));
 
   useEffect(() => {
-    console.log(data);})
+    console.log(data);
 
-  //   setDiaryName(data?.title!);
+    setDiaryName(data?.title!);
 
-  //   if (data.diaries.find(isUserDiary)) {
-  //     setUserDiary({
-  //       title: data?.title!,
-  //       id: data?.diaries.find(isUserDiary)!.id,
-  //       nickname: data?.diaries.find(isUserDiary)!.user.nickname,
-  //       content: data?.diaries.find(isUserDiary)!.content,
-  //     });
-  //   }
+    if (data?.diaries?.find(isUserDiary)) {
+      setUserDiary({
+        title: data?.title!,
+        id: data?.diaries.find(isUserDiary)!.id,
+        nickname: data?.diaries.find(isUserDiary)!.user.nickname,
+        content: data?.diaries.find(isUserDiary)!.content,
+      });
+    }
     
-  //   if (data.diaries.find(isPartnerDiary)) {
-  //     setPartnerDiary({
-  //       title: data?.title!,
-  //       id: data?.diaries.find(isPartnerDiary)!.id,
-  //       nickname: data?.diaries.find(isPartnerDiary)!.user.nickname,
-  //       content: data?.diaries.find(isPartnerDiary)!.content,
-  //     });
-  //   }
-  // }, [data]);
+    if (data?.diaries?.find(isPartnerDiary)) {
+      setPartnerDiary({
+        title: data?.title!,
+        id: data?.diaries.find(isPartnerDiary)!.id,
+        nickname: data?.diaries.find(isPartnerDiary)!.user.nickname,
+        content: data?.diaries.find(isPartnerDiary)!.content,
+      });
+    }
+  }, [data]);
 
-  // return { diaryName, userDiary, partnerDiary };
-  return data;
+  return { diaryName, userDiary, partnerDiary };
 }
