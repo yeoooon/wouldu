170894d9b182b635f6c3831855a799ecabc9c207
taewon <stw8194@google.com>
2022-12-12 13:54:10 +0900

    fix:sub_model

--- ai/sub_model.py
+++ ai/sub_model.py
@@ -3,22 +3,20 @@ import tensorflow as tf
 import pickle
 import os
 
-config = tf.ConfigProto(intra_op_parallelism_threads=8,
-                        inter_op_parallelism_threads=0,
-                        allow_soft_placement=True,
-                        device_count = {'GPU': 1})
-sess = tf.Session(config=config)
-
-load_folder_path = 'saved_model35'
-
-with open(os.path.join(load_folder_path, 'tags_vectorizer.pkl'), 'rb') as handle:
-    tags_vectorizer = pickle.load(handle)
-    slots_num = len(tags_vectorizer.label_encoder.classes_)
-with open(os.path.join(load_folder_path, 'intents_label_encoder.pkl'), 'rb') as handle:
-    intents_label_encoder = pickle.load(handle)
-    intents_num = len(intents_label_encoder.classes_)
-
 def sub_model(x):
+    config = tf.ConfigProto(intra_op_parallelism_threads=8,
+                            inter_op_parallelism_threads=0,
+                            allow_soft_placement=True,
+                            device_count = {'GPU': 1})
+    sess = tf.Session(config=config)
+
+    load_folder_path = 'saved_model35'
+
+    with open(os.path.join(load_folder_path, 'tags_vectorizer.pkl'), 'rb') as handle:
+        tags_vectorizer = pickle.load(handle)
+    with open(os.path.join(load_folder_path, 'intents_label_encoder.pkl'), 'rb') as handle:
+        intents_label_encoder = pickle.load(handle)
+
     model = JointBertModel.load(load_folder_path, sess)
     with sess.as_default():
         with sess.graph.as_default():
