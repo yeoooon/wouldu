69f209e3c840d3fa480b00fac81856fc6ed002ce
taewon <stw8194@google.com>
2022-12-12 11:45:32 +0900

    fix:sess

--- ai/sub_model.py
+++ ai/sub_model.py
@@ -3,6 +3,8 @@ from models.joint_bert import JointBertModel
 def sub_model(x, tags_vectorizer, intents_label_encoder, sess):
     load_folder_path = 'saved_model35'
     model = JointBertModel.load(load_folder_path, sess)
-    _, intent, _, _, _, _ = model.predict_slots_intent(x, tags_vectorizer, intents_label_encoder)
+    with sess.as_default():
+        with sess.graph.as_default():
+            _, intent, _, _, _, _ = model.predict_slots_intent(x, tags_vectorizer, intents_label_encoder)
     
     return int(intent[0].strip())
