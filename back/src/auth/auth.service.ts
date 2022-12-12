10ec806bef76cd0e6fd6d4af01661a9f8dd8f098
taewon <stw8194@google.com>
2022-12-12 23:46:58 +0900

    feat:kakao

--- back/src/auth/auth.service.ts
+++ back/src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import * as bcrypt from 'bcrypt';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
@@ -7,6 +7,8 @@ import { Repository } from 'typeorm';
 import { JwtService } from '@nestjs/jwt';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
+import * as uuid from 'uuid';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
@@ -48,28 +50,38 @@ export class AuthService {
   }
 
   async kakao(code: string) {
-    const kakao_api_url = `https://kauth.kakao.com/oauth/token
-    ?grant_type=authorization_code
-    &client_id=${process.env.KAKAO_CLIENT_ID}
-    &redirect_url=${process.env.KAKAO_REDIRECT_URL}
-    &code=${code}`;
-    console.log(kakao_api_url);
+    const kakao_api_url = `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${process.env.KAKAO_CLIENT_ID}&redirect_url=${process.env.KAKAO_REDIRECT_URL}&code=${code}`;
 
     const token_res = await firstValueFrom(
       this.httpService.post(kakao_api_url),
     );
-    console.log(token_res);
+
     const accessToken: string = token_res.data.access_token;
-    console.log(accessToken);
-    // const userInfo = await firstValueFrom(
-    //   this.httpService.get('https://kapi.kakao.com/v2/user/me', {
-    //     headers: {
-    //       Authorization: `Bearer ${accessToken}`,
-    //     },
-    //   }),
-    // );
-    // console.log(userInfo);
-    // const userId: string = userInfo.data.id;
-    // console.log(userId);
+    const userInfo = await firstValueFrom(
+      this.httpService.get('https://kapi.kakao.com/v2/user/me', {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }),
+    );
+    const email =
+      userInfo.data.kakao_account.email ?? userInfo.data.id + '@kakao.com';
+    console.log(email);
+    const data = await this.userRepository.findOne({ where: { email: email } });
+    console.log(data);
+    if (!data) {
+      const user = new User();
+
+      const signupVerifyToken = uuid.v4();
+      user.email = email;
+      user.nickname = data.nickname;
+      user.hashedPassword = 'kakao';
+      user.socialId = 'kakao' ?? null;
+      user.profileImgUrl = null;
+      user.signupVerifyToken = signupVerifyToken;
+      user.registerProgress = 1;
+      user.friendCode = await this.userService.makeFriendCode();
+      await this.userRepository.save(user);
+    }
   }
 }
