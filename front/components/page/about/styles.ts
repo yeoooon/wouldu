7122b0ba32ffc58a0ce6fd93e9fff9edc37dff84
남궁혜진 <nkhj0404@gmail.com>
2022-12-14 12:30:50 +0900

    fix: about variant 조금 변경

--- front/components/page/about/styles.ts
+++ front/components/page/about/styles.ts
@@ -2,29 +2,33 @@ import { Variants } from "framer-motion";
 
 export const cardVariants: Variants = {
   offscreen: {
-    x: -150
+    x: -30,
+    opacity: 0,
   },
   onscreen: {
-    x: 50,
+    x: 0,
+    opacity: 1,
     rotate: 0,
     transition: {
       type: "spring",
-      bounce: 0.3,
-      duration: 0.5
-    }
-  }
+      bounce: 0.1,
+      duration: 0.8,
+    },
+  },
 };
 export const TextVariants: Variants = {
   offscreen: {
-    y: 150
+    y: 150,
+    opacity: 0,
   },
   onscreen: {
     y: 50,
+    opacity: 1,
     rotate: 0,
     transition: {
       type: "spring",
-      bounce: 0.3,
-      duration: 0.6
-    }
-  }
+      bounce: 0.1,
+      duration: 0.8,
+    },
+  },
 };
