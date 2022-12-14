9e1accadaac9a4600b5c3d5719fef616c5a95557
yeoooon <katkrar@naver.com>
2022-12-14 17:12:06 +0900

    feat: 추천 활동 있을 경우 처리

--- front/components/page/planner/Suggesttodo.tsx
+++ front/components/page/planner/Suggesttodo.tsx
@@ -1,17 +1,42 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { useRecoilValue } from "recoil";
+import { formatDate } from "@services/utils/formatDate";
+
 import { CheckIcon } from "@components/icons/CheckIcon";
 import { Box, Container } from "@styles/layout";
-import React from "react";
-import styled from "styled-components";
 import TodoItem from "./TodoItem";
+import usePlanQuery from "@services/utils/usePlanQuery";
+import { Planner } from "@type/planner";
+import { dayAtom } from "@recoil/planner";
 
 const Suggesttodo = () => {
+  const [recommendTodo, setRecommendTodo] = useState<Planner | null>(null);
+
+  const recoilDay = useRecoilValue<Date>(dayAtom);
+  const day = formatDate(recoilDay);
+  const { data: planData } = usePlanQuery(day);
+
+  useEffect(() => {
+    if (planData?.find((todo: Planner) => todo.isRecommended === 1)) {
+      setRecommendTodo(planData?.find((todo: Planner) => todo.isRecommended === 1));
+    } else {
+      setRecommendTodo(null);
+    }
+  }, [planData]);
+
   return (
     <SuggestContainer>
       <TitleBox>
         <CheckIcon />
         <Text>추천 활동</Text>
       </TitleBox>
-      <TodoItem description="감정분석을 통한 해야할일" isCompleted={0} />
+      {recommendTodo?
+        <><TodoItem description={recommendTodo.description} isCompleted={0} /></>
+        :
+        <><p>아직 추천 활동이 없어요!</p></>
+      }
+      
     </SuggestContainer>
   );
 };
